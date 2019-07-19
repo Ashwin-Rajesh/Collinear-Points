c3002c4c2029b1bace915a34ba314b682f06c5d6
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-19 18:33:06 +0530

    Replaced repeated code for entering points and displaying points in main() with functions

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,34 +1,39 @@
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
+    // Stores data of collinear lines found
     private int ln_no;
     private LineSegment[] lines;
 
     public BruteCollinearPoints(Point[] arg_points)
     {
-        // To handle case of passed array having less than 4 elements
-        if(arg_points.length < 4)
-            throw new IllegalArgumentException(" Array with less than 4 elements was passed.");
+        // To handle pass of null argument
+        if(arg_points == null)
+            throw new IllegalArgumentException(" Argument passed was null");
 
+        Point[] points = arg_points.clone();
         final int pt_no = arg_points.length;
-        Point[] points = arg_points;
-        lines = new LineSegment[pt_no / 2 - 1];
+        lines = new LineSegment[pt_no * (pt_no - 1) / 2];
         ln_no = 0;
+
+        // To handle occurrence of null values in argument array
+        for(int i= 0; i < pt_no; i++)
+            if(points[i] == null)
+                throw new IllegalArgumentException(" Array with null elements was passed.");
 
         Arrays.sort(points,Point::compareTo);
 
-        // To handle occurrence of duplicate or null points
-        for(int i = 0; i < pt_no; i++) {
-            if(points[i] == null)
-                throw new IllegalArgumentException(" Array with null elements was passed.");
+        // To handle occurrence of duplicate
+        for(int i = 0; i < pt_no; i++)
             if (i < pt_no - 1)
                 if (points[i].slopeTo(points[i + 1]) == Double.NEGATIVE_INFINITY)
                     throw new IllegalArgumentException(" Duplicate points are not permitted.");
-        }
+
+        // To handle argument being passed with less than 4 elements.
+        if(pt_no < 4)
+            return;
 
         // To detect 4 - point long line segments by iterating through all possible combinations.
         for(int i = 0; i < pt_no - 3; i++)
@@ -57,82 +62,24 @@ public class BruteCollinearPoints {
 
     public LineSegment[] segments()
     {
-        LineSegment r_lines[] = new LineSegment[ln_no];
+        LineSegment[] r_lines = new LineSegment[ln_no];
         for(int i = 0; i < ln_no; i++)  r_lines[i] = lines[i];
         return r_lines;
     }
 
     public static void main(String[] args)
     {
-        final int num_pt = Integer.parseInt(args[0]);
-        int num_ln;
-        int max_coord = 0;
-        BruteCollinearPoints brute;
-        Point[] points = new Point[num_pt];
+        // Retrieving points
+        Point[] inp_points = Point.getpoints(args);
 
-        for(int i = 0; i < num_pt; i++)
+        BruteCollinearPoints collinearPoints = new BruteCollinearPoints(inp_points);
+        StdOut.println("There are " + collinearPoints.numberOfSegments() + " collinear lines in the sample. They are :");
+        for (int i= 0; i < collinearPoints.numberOfSegments(); i++)
         {
-            int x_coord, y_coord;
-            StdOut.print(" Enter x-coordinate of point number " + (i+1) + " : ");
-            x_coord = StdIn.readInt();
-            StdOut.print(" Enter y-coordinate of point number " + (i+1) + " : ");
-            y_coord = StdIn.readInt();
-            points[i] = new Point(x_coord,y_coord);
-            StdOut.println();
-            if(-x_coord > max_coord)     max_coord = -x_coord;
-            if(x_coord > max_coord)     max_coord = x_coord;
-            if(-y_coord > max_coord)     max_coord = -y_coord;
-            if(y_coord > max_coord)     max_coord = y_coord;
-        }
-        StdOut.println("-----------------------------------------------------------------------------");
-
-        Arrays.sort(points,Point::compareTo);
-
-        StdOut.println(" These are the points - ");
-        for(int i = 0; i < num_pt; i++)
-        {
-            StdOut.println(points[i].toString());
-        }
-        StdOut.println("-----------------------------------------------------------------------------");
-
-        brute = new BruteCollinearPoints(points);
-
-        num_ln = brute.numberOfSegments();
-        LineSegment[] lines = brute.segments();
-
-        StdOut.println(" There are " + num_ln + " collinear lines in the given sample.");
-        StdOut.println("-----------------------------------------------------------------------------");
-
-        StdOut.println(" These are the lines:");
-        for(int i = 0; i < num_ln; i++)
-        {
-            StdOut.println(lines[i].toString());
+            StdOut.println(collinearPoints.segments()[i]);
         }
 
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(-max_coord - (double)max_coord/20, max_coord + (double)max_coord/20);
-        StdDraw.setYscale(-max_coord - (double)max_coord/20, max_coord + (double)max_coord/20);
-
-        // Drawing axes
-        StdDraw.setPenColor(128,128,128);
-        new Point(0, max_coord).drawTo(new Point(0, -max_coord));
-        new Point(max_coord, 0).drawTo(new Point(-max_coord, 0));
-
-        // Drawing all possible lines between points
-        StdDraw.setPenColor(0,0,0);
-        for(int i = 0; i < num_pt; i++)
-        {
-            for(int j= i + 1; j < num_pt; j++) {
-                points[i].drawTo(points[j]);
-            }
-        }
-
-        // Drawing all points
-        StdDraw.setPenRadius((double)max_coord/200);
-        for(int i = 0; i < num_pt; i++)
-        {
-            points[i].draw();
-        }
-        StdDraw.show();
+        // Draws all points using StdDraw
+        Point.drawpoints(inp_points,10);
     }
 }
