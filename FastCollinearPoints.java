7dde4e023ad366ece9d36fef3110f6df32a69dd8
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-19 18:37:18 +0530

    Replaced repeating code with functions and fixed issue with detection of coll.lines with points >4
    Fixes #1

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,140 +1,100 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-
 import java.util.Arrays;
 
 public class FastCollinearPoints {
 
+    // Stores data of collinear lines found.
     private int ln_no;
     private LineSegment[] lines;
 
-    public FastCollinearPoints(Point[] points)
+    // Constructor
+    public FastCollinearPoints(Point[] arg_points)
     {
-        final int pt_no = points.length;
-        ln_no = 0;
+        // To handle pass of null argument
+        if(arg_points == null)
+            throw new IllegalArgumentException(" Argument passed was null");
+
+        Point[] points = arg_points.clone();
+        final int pt_no = arg_points.length;
         lines = new LineSegment[pt_no * (pt_no - 1) / 2];
+        ln_no = 0;
+
+        // To handle occurrence of null values in argument array
+        for(int i= 0; i < pt_no; i++)
+            if(points[i] == null)
+                throw new IllegalArgumentException(" Array with null elements was passed.");
 
         Arrays.sort(points,Point::compareTo);
 
-        // To occurrence of duplicate points
+        // To handle occurrence of duplicate
         for(int i = 0; i < pt_no; i++)
-            if(i < pt_no -1)
-                if(points[i].slopeTo(points[i+1]) == Double.NEGATIVE_INFINITY)
+            if (i < pt_no - 1)
+                if (points[i].slopeTo(points[i + 1]) == Double.NEGATIVE_INFINITY)
                     throw new IllegalArgumentException(" Duplicate points are not permitted.");
 
-        for(int i = 0; i < pt_no - 3; i++)
-        {
-            Point[] sample_points = Arrays.copyOfRange(points, i + 1, pt_no);
-            Arrays.sort(sample_points,points[i].slopeOrder());
+        // To handle argument being passed with less than 4 elements.
+        if(pt_no < 4)
+            return;
 
-            for(int j = 0; j < pt_no - i -3; j++)
+        for (int i = 0; i < pt_no; i++)
+        {
+            Point[] sample_points = points.clone();
+            Arrays.sort(sample_points, points[i].slopeOrder());
+
+            for(int j = 1; j < pt_no - 1; j++)
             {
-                if(sample_points[j].slopeTo(points[i]) == sample_points[j + 1].slopeTo(points[i]) && sample_points[j].slopeTo(points[i]) == sample_points[j + 2].slopeTo(points[i]))
+                int ln_len = 1;                 // Variable stores length of line
+                boolean val_flag = true;        // Variable stores if the line has been read before (if it contains a point < origin point)
+
+                if(sample_points[0].compareTo(sample_points[j]) > 0)        val_flag = false;
+                while(sample_points[0].slopeTo(sample_points[j]) == sample_points[0].slopeTo(sample_points[j + 1]))
                 {
-                    lines[ln_no] = new LineSegment(points[i], sample_points[j + 2]);
+                    j++;
+                    ln_len++;
+
+                    // Invalidate the line if a point that was checked before is found on the line.
+                    if(sample_points[0].compareTo(sample_points[j]) > 0)
+                        val_flag = false;
+
+                    if(j == pt_no - 1)
+                        break;
+                }
+
+                if(val_flag && ln_len >= 3)
+                {
+                    lines[ln_no] = new LineSegment(sample_points[0],sample_points[j]);
                     ln_no++;
                 }
             }
         }
     }
 
-    public int numberOfSegments()       { return ln_no;}
+    // Returns number of lines found
+    public int numberOfSegments() {
+        return ln_no;
+    }
 
+    // Returns all lines found in the form of LineSegment objects
     public LineSegment[] segments()
     {
         LineSegment r_lines[] = new LineSegment[ln_no];
-        for(int i = 0; i < ln_no; i++)  r_lines[i] = lines[i];
+        for (int i = 0; i < ln_no; i++) r_lines[i] = lines[i];
         return r_lines;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        // Retrieving points
+        Point inp_points[] = Point.getpoints(args);
 
-        int max_coord = 0;
-        // This code retrieves information about points from a file.
-        In in = new In(args[0]);
-        int num_pt = in.readInt();
-        Point[] points = new Point[num_pt];
-
-        StdOut.println(" Loaded file - " + args[0] + " . Reading file for " + num_pt + "points.");
-        for (int i = 0; i < num_pt; i++) {
-            int x_coord = in.readInt();
-            int y_coord = in.readInt();
-            points[i] = new Point(x_coord, y_coord);
-            if (-x_coord > max_coord) max_coord = -x_coord;
-            if (x_coord > max_coord) max_coord = x_coord;
-            if (-y_coord > max_coord) max_coord = -y_coord;
-            if (y_coord > max_coord) max_coord = y_coord;
-            StdOut.print((i+1));
-        }
-        /*
-        // This code retrieves information about points from StdIn.
-        int num_pt = args[0];
-        int max_coords = 0;
-        Point[] points = new Point[num_pt];
-
-        for (int i = 0; i < num_pt; i++) {
-            int x_coord, y_coord;
-            StdOut.print(" Enter x-coordinate of point number " + (i + 1) + " of " + num_pt + " : ");
-            x_coord = StdIn.readInt();
-            StdOut.print(" Enter y-coordinate of point number " + (i + 1) + " of " + num_pt + " : ");
-            y_coord = StdIn.readInt();
-            points[i] = new Point(x_coord, y_coord);
-            StdOut.println();
-            if (-x_coord > max_coord) max_coord = -x_coord;
-            if (x_coord > max_coord) max_coord = x_coord;
-            if (-y_coord > max_coord) max_coord = -y_coord;
-            if (y_coord > max_coord) max_coord = y_coord;
-        }
-        */
-        StdOut.println("-----------------------------------------------------------------------------");
-
-        Arrays.sort(points, Point::compareTo);
-
-        StdOut.println(" These are the points - ");
-        for (int i = 0; i < num_pt; i++) {
-            StdOut.println(points[i].toString());
-        }
-        StdOut.println("-----------------------------------------------------------------------------");
-
-        FastCollinearPoints Collinear_points = new FastCollinearPoints(points);
-        final int num_ln = Collinear_points.numberOfSegments();
-        LineSegment[] lines = Collinear_points.segments();
-
-        StdOut.println(" There are " + num_ln + " collinear lines in the given sample.");
-        StdOut.println("-----------------------------------------------------------------------------");
-
-        StdOut.println(" These are the lines:");
-        for (int i = 0; i < num_ln; i++) {
-            StdOut.println(lines[i].toString());
-        }
-
-        // Initialising StdDraw
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(-max_coord - (double) max_coord / 20, max_coord + (double) max_coord / 20);
-        StdDraw.setYscale(-max_coord - (double) max_coord / 20, max_coord + (double) max_coord / 20);
-
-        // Drawing axes
-        StdDraw.setPenColor(128, 128, 128);
-        new Point(0, max_coord).drawTo(new Point(0, -max_coord));
-        new Point(max_coord, 0).drawTo(new Point(-max_coord, 0));
-
-        // Drawing all possible lines between points
-        StdDraw.setPenColor(0, 0, 0);
-        for (int i = 0; i < num_pt; i++) {
-            for (int j = i + 1; j < num_pt; j++) {
-                points[i].drawTo(points[j]);
-            }
-        }
-
-        // Drawing all points
-        StdDraw.setPenColor(255,43,55);
-        StdDraw.setPenRadius((double)max_coord/200);
-        for(int i = 0; i < num_pt; i++)
+        FastCollinearPoints collinearPoints = new FastCollinearPoints(inp_points);
+        StdOut.println("There are " + collinearPoints.numberOfSegments() + " collinear lines in the sample. They are :");
+        for (int i= 0; i < collinearPoints.numberOfSegments(); i++)
         {
-            points[i].draw();
+            StdOut.println(collinearPoints.segments()[i]);
         }
-        StdDraw.show();
+
+        // Drawing all points using StdDraw
+        Point.drawpoints(inp_points,10);
     }
 }
